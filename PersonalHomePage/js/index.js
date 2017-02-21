1dec595a94e5508a5a5c16b17f1dceb7f957ab76
biluweb <bilu.verygood@aliyun.com>
2017-02-21 20:46:22 +0800

    add vue

--- PersonalHomePage/js/index.js
+++ PersonalHomePage/js/index.js
@@ -1,69 +1,107 @@
-$(function(){
-
-		//鑾峰彇瀵艰埅鐨勬爣鍑嗘祦绂籨ocument鐨�0锛�0鐐圭殑璺濈..
-		function navmh(){
-			var bzliu=$('nav').offset().top;
-		    $(window).scroll(function(event){
-		        if($(window).scrollTop()>=bzliu){
-		            $('nav').addClass('navfixed');
-	            	$('.head').addClass('headfixed');
-	            	var be=bzliu;
-	            	$('.headfixed').css({'top':-be})
-		        }else{$('nav').removeClass('navfixed');
-		        	$('.head').removeClass('headfixed')
-		        	$('.head').css({'top':0})
+define('main',['jquery','vue','mobileDevice'], function ($,Vue){
+	//vue
+	var vm=new Vue({
+		el:'#app',
+	  	data: {
+			resume:null,
+			navcurt:null
+	  	},
+	  	created: function () {
+	  		// `this` 指向 vm 实例
+	  		
+	  		//url截取
+	  		var url = window.location.href,
+				loc = url.substring(url.lastIndexOf('/')+1, url.length);
+	  		this.navcurt=loc;
+	  		
+		    // `数据 交互
+		    var _this=this;
+		    $.ajax({
+		    	type:"get",
+		    	url:"/code/Api/myinfor/resume.json",
+		    	async:false,
+		    	success:function(data, textStatus){
+		    		_this.resume=data.resume[0];
 		    	}
-	
-		        if($(window).scrollTop()>bzliu){
-		            $('.main').addClass('relemain');
-		            var bn=bzliu;
-		            $('.relemain').css({'top':bn+$('nav').height()})
-		        }else{
-		            $('.main').removeClass('relemain');
-		            $('.main').css({'top':$('nav').height()})
-		        }
-		    })
-		}
-
-	    setTimeout(function(){navmh();},301)
-	    
-//	    loading
-		function loadingFn(){
-		var num=0;
-		var arry=['img/my.png','img/stripes.png','img/myname.png','img/fash.png','img/footer.png','img/or.png','img/section_home_fg.png','img/yuan.png','img/bgindex.jpg','icon/BIL.png','icon/jiant.png','icon/mous.png'];
-		for(var i=0; i<arry.length; i++){
-				var imgObj=new Image();
-				imgObj.src='image/'+arry[i];
-				imgObj.onload=function(){
-					num++;
-					var s=parseInt((num/arry.length)*100);
-					$('.progress-word').html(s+'%');
-					$('.progress-bar').width(s+'%');
-					if(s>=0){$('.progress').attr('class','progress d5')}
-					if(s>=8){$('.progress').attr('class','progress d25')}
-					if(s>=25){$('.progress').attr('class','progress d50')}
-					if(s>=50){$('.progress').attr('class','progress d75')}
-					if(s>=75){$('.progress').attr('class','progress d100')}
-					if(num==12){
-						$('.progress-bar').remove();
-						$('.progressbg').fadeOut(1000,function () {
-							$('.progressbg').remove();
-							$('body').css('overflow','auto');
-						})
+		    });
+		    
+		    //交互
+		    $(function(){
+				//鑾峰彇瀵艰埅鐨勬爣鍑嗘祦绂籨ocument鐨�0锛�0鐐圭殑璺濈..
+				function navmh(){
+					var bzliu=$('nav').offset().top;
+				    $(window).scroll(function(event){
+				        if($(window).scrollTop()>=bzliu){
+				            $('nav').addClass('navfixed');
+			            	$('.head').addClass('headfixed');
+			            	var be=bzliu;
+			            	$('.headfixed').css({'top':-be})
+				        }else{$('nav').removeClass('navfixed');
+				        	$('.head').removeClass('headfixed')
+				        	$('.head').css({'top':0})
+				    	}
+			
+				        if($(window).scrollTop()>bzliu){
+				            $('.main').addClass('relemain');
+				            var bn=bzliu;
+				            $('.relemain').css({'top':bn+$('nav').height()})
+				        }else{
+				            $('.main').removeClass('relemain');
+				            $('.main').css({'top':$('nav').height()})
+				        }
+				    })
+				}
+		
+			    setTimeout(function(){navmh();},301)
+			    
+				//	    loading
+				function loadingFn(){
+				var num=0;
+				var arry=['img/my.png','img/stripes.png','img/myname.png','img/fash.png','img/footer.png','img/or.png','img/section_home_fg.png','img/yuan.png','img/bgindex.jpg','icon/BIL.png','icon/jiant.png','icon/mous.png'];
+				for(var i=0; i<arry.length; i++){
+						var imgObj=new Image();
+						imgObj.src='image/'+arry[i];
+						imgObj.onload=function(){
+							num++;
+							var s=parseInt((num/arry.length)*100);
+							$('.progress-word').html(s+'%');
+							$('.progress-bar').width(s+'%');
+							if(s>=0){$('.progress').attr('class','progress d5')}
+							if(s>=8){$('.progress').attr('class','progress d25')}
+							if(s>=25){$('.progress').attr('class','progress d50')}
+							if(s>=50){$('.progress').attr('class','progress d75')}
+							if(s>=75){$('.progress').attr('class','progress d100')}
+							if(num==12){
+								$('.progress-bar').remove();
+								$('.progressbg').fadeOut(1000,function () {
+									$('.progressbg').remove();
+									$('body').css('overflow','auto');
+								})
+							}
+						}
+						
+						imgObj.onerror=function(){
+							$('.progress-bar').width(100+'%');
+							$('.progress-word').html('100%');
+							$('.progress-bar').remove();
+							$('.progressbg').fadeOut(1000,function () {
+								$('.progressbg').remove();
+								$('body').css('overflow','auto');
+							})
+						}
 					}
 				}
-				
-				imgObj.onerror=function(){
-					$('.progress-bar').width(100+'%');
-					$('.progress-word').html('100%');
-					$('.progress-bar').remove();
-					$('.progressbg').fadeOut(1000,function () {
-						$('.progressbg').remove();
-						$('body').css('overflow','auto');
-					})
-				}
-			}
-		}
-		loadingFn();
+				loadingFn();
+			})
+		    
+	  	},
+	})
+	
+	//Device
+	if(isMobileDevice()){
+		$('head').append("<link rel='stylesheet' type='text/css' href='css/myMobile.css' />");
+	}
+}),
 
-})
+require(['main'], function (){console.log('load js ok!');console.log('不是因为我不上天，而是因为我在云端')});
+
---
+++ PersonalHomePage/js/requireconfig.js
@@ -0,0 +1,9 @@
+require.config({
+　　　　paths: {
+　　　　　　"jquery":"js/jquery-1.11.3.min",
+　　　　　　"game":"js/game",
+　　　　　　"jquery.mousewheel":"js/jquery.mousewheel.min",
+		    "mobileDevice":"js/mobileDevice",
+		    "vue":"https://unpkg.com/vue/dist/vue",
+　　　　}
+});
