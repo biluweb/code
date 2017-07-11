38446671a33f56012dbdca9d0936651658f5c776
biluweb <bilu.verygood@aliyun.com>
2017-07-11 14:47:31 +0800

    add functions.js

---
+++ PersonalHomePage/js/functions.js
@@ -0,0 +1,11 @@
+ //计算年龄
+ function ages(str){
+    var   r   =   str.match(/^(\d{1,4})(-|\/)(\d{1,2})\2(\d{1,2})$/);     
+    if(r==null)return   false;     
+    var   d=  new   Date(r[1],   r[3]-1,  r[4]);     
+    if(d.getFullYear()==r[1]&&(d.getMonth()+1)==r[3]&&d.getDate()==r[4]){   
+         var   Y  =  new Date().getFullYear();   
+         return("年龄:"+   (Y-r[1]));   
+    }   
+    return("输入的日期格式错误！");
+ }   
